a571772552b995e602cc59ef97a7034c27a1760c
ServetC <489394@student.fontys.nl>
2022-10-18 14:12:42 +0200

    Front end changes signin siggnup rooter

--- front end/bas_world_design/src/App.js
+++ front end/bas_world_design/src/App.js
@@ -1,35 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route
+  } from "react-router-dom";
+import Login from "./pages/login";
+import MainPage from "./pages/mainpage";
+import Signup from "./pages/signup"
 
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <input className="SearchBar" type="text"></input>
-                <div className="LogIn">Login</div>
-                <div className="header">
-                    <div className="categories">Vehicle categories</div>
-                    <div className="contact">Contact</div>
-                    <div className="sell">Sell your vehicle</div>
-                    <div className="buy_info">How to buy</div>
-                </div>
-                <div className="vehicle">
-                    <p className="green">Buy your vehicle online.</p>
-                    <p className="white"> Easy and safe</p>
-                    <div className="filter">
-                        <div className="type">Select vehicle type</div>
-                        <div className="filters">Brand</div>
-                        <div className="filters">Model</div>
-                        <div className="filters">Year From To</div>
-                        <div className="filters">$ From To</div>
-                        <button className="results">Show results</button>
-                    </div>
-                </div>
-            </header>
+function App (){
+    return(
 
-        </div>
-    );
+      <div>
+          <Router>             
+                <Routes>
+                  <Route path="/signup" element={<Signup/>}/>
+                  <Route path="/login" element={<Login/>}/>
+                  <Route path="/" element={<MainPage/>}/>
+                </Routes>              
+            </Router>
+      </div>          
+          );
 }
-
 export default App;
