d6781f914052148a9e0474e4605d318b5eab2dd7
Martin Todorov <i481796@fhict.nl>
2022-11-21 22:52:49 +0100

    bot improvements and frontend improvements

--- my-app/src/pages/Chatbot/config.js
+++
@@ -1,8 +0,0 @@
-import { createChatBotMessage } from "react-chatbot-kit";
-import Options from "../../components/Options/Options";
-const config = {
-  botName: "BasWorldChatBot",
-  initialMessages: [createChatBotMessage(`Hello world`)],
-};
-
-export default config;
