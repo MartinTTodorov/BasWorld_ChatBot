a9b5a3788ad79201bbc5f09dbea1fd28fe054a7b
Martin Todorov <i481796@fhict.nl>
2022-12-13 03:15:12 +0100

    major improvements

--- front end/bas_world_design/src/Greeter.js
+++ front end/bas_world_design/src/Greeter.js
@@ -6,11 +6,12 @@ import "./Greeter.css";
 import ChatBot from 'react-simple-chatbot';
 
 
-
 const Greeter = () => {
     // const [message, setMessage] = useState(`Hello ${greeter(name)}, how can I help you today?`);
     const [display, setDisplay] = useState(false);
     const [name, setName] = useState(sessionStorage.getItem("userName"));
+    const [userMessage, setUserMessage] = useState("");
+    const [botResponse, setResponse] = useState("");
    //const navigate = useNavigate();
 
     useEffect(() => {
@@ -25,6 +26,7 @@ const Greeter = () => {
         }
         return name;
     }
+
 
 
     // const saveMessage = (e) => {
@@ -51,96 +53,154 @@ const Greeter = () => {
         setDisplay(!display);
     }
    
+    
+
 const saveConvo = (index, message, sender, receiver) => {
     axios.post('http://localhost:8080/conversations', {message:  message, sender: sender, receiver: receiver})
 }
 
 
+const saveMessage = () =>{
+    //console.log(steps[3].id);
+    //return steps[3];
+    console.log(steps)
+}
 
 
+function Tests(input){
+
+    console.log(input);
+    return {input};
+}
+
+
+
+var idNumber = 1;
+var idNextStep = idNumber+1;
+
+
+// function asd() {
+//     idNumber+=1;
+//     steps.push(
+//         {
+//             id: idNumber,
+//             user: true,
+//             trigger: idNextStep,
+//         }
+//     )
+
+
+// }
+
+    
+
+    
     let steps = [];
         steps=[
             {
-              id: '1',
+              id: 1,
               message: `Hello ${loggedUser()} ! My name is Bassy the chat bot! Here are some frequently asked questions?`,
-              trigger: '2',
+              trigger: 2,
             },
             {
-              id: '2',
-              options: [
-                { value: 1, label: 'FAQ 1', trigger: '3' },
-                { value: 2, label: 'FAQ 2', trigger: '4' },
-                { value: 3, label: 'FAQ 3', trigger: '5' },
-                { value: 4, label: 'Neither', trigger: '6' },
-              ],
-            },
-            
-            {
-              id: '3',
-              message: 'FAQ 1 answer!',
-              trigger: '8'
+                id: 2,
+                user:true,
+                trigger: '3',
             },
             {
-              id: '4',
-              message: 'FAQ 2 answer!',
-              trigger: '8'
+                id: '3',
+                message: 'hello',
+                trigger: '4',
             },
             {
-              id: '5',
-              message: 'FAQ 3 answer!',
-                trigger: '8'
+                id: '4',
+                user: true,
+                trigger: '5',
+                validator: Tests,
             },
             {
-                id: '6',
-                message: 'Please choose a topic!',
-                trigger: '7'
-                  
-            },
+                id: '5',
+                user: true,
+                asMessage: true,
+                end: true
+            }
             // {
-            //     id: 'extra',
-            //     options:[
-            //         { value: 1, label: 'Yes', trigger: '2' },
-            //         { value: 2, label: 'No', trigger: '10' },
-            //     ],
-            // }
+            //   id: '2',
+            //   options: [
+            //     { value: 1, label: 'FAQ 1', trigger: '3' },
+            //     { value: 2, label: 'FAQ 2', trigger: '4' },
+            //     { value: 3, label: 'FAQ 3', trigger: '5' },
+            //     { value: 4, label: 'Neither', trigger: '6' },
+            //   ],
+            // },
+            
+            // {
+            //   id: '3',
+            //   message: 'FAQ 1 answer!',
+            //   trigger: '8'
+            // },
+            // {
+            //   id: '4',
+            //   message: 'FAQ 2 answer!',
+            //   trigger: '8'
+            // },
+            // {
+            //   id: '5',
+            //   message: 'FAQ 3 answer!',
+            //     trigger: '8'
+            // },
+            // {
+            //     id: '6',
+            //     message: 'Please choose a topic!',
+            //     trigger: '7'
+                  
+            // },
+            // // {
+            // //     id: 'extra',
+            // //     options:[
+            // //         { value: 1, label: 'Yes', trigger: '2' },
+            // //         { value: 2, label: 'No', trigger: '10' },
+            // //     ],
+            // // }
         ]
+
+         
             
             
-            
-            steps.push(
-                {
-                    id: '7',
-                    options: [
+            // steps.push(
+            //     {
+            //         id: '7',
+            //         options: [
                         
-                    ]
-                }
-                )
+            //         ]
+            //     }
+            //     )
                 
-                for (let index = 1; index < 5; index++) {
-                    steps[6].options.push(
-                        { value: index, label: 'Topic ' + index, trigger: '8' }
-                    )
-                }
+            //     for (let index = 1; index < 5; index++) {
+            //         steps[6].options.push(
+            //             { value: index, label: 'Topic ' + index, trigger: '8' }
+            //         )
+            //     }
             
-            steps.push(
-                {
-                    id: '8',
-                    message: 'Do you have any more questions?',
-                    trigger: '9'
-                },
-                {
-                    id: '9',
-                    options:[
-                        { value: 1, label: 'Yes', trigger: '6' },
-                        { value: 2, label: 'No', trigger: '10' },
-                    ],
-                },
-                {
-                    id: '10',
-                    message: 'Thank you for reaching out to Bassy! Have a great day!',
-                    end: true
-                }
-            )
+            // steps.push(
+            //     {
+            //         id: '8',
+            //         message: 'Do you have any more questions?',
+            //         trigger: '9'
+            //     },
+            //     {
+            //         id: '9',
+            //         options:[
+            //             { value: 1, label: 'Yes', trigger: '6' },
+            //             { value: 2, label: 'No', trigger: '10' },
+            //         ],
+            //     },
+            //     {
+            //         id: '10',
+            //         message: 'Thank you for reaching out to Bassy! Have a great day!',
+            //         end: true
+            //     }
+            // )
         
     
 
@@ -156,7 +216,8 @@ const saveConvo = (index, message, sender, receiver) => {
         <>
             <div className="chat-container">
                 <segment className={display ? "coversation chatbox" : "normal-coversation"}>
-                    <ChatBot steps={steps}/>
+                    <ChatBot steps={steps} cache={true}/>
+                    
                 </segment>
                 <button className="popButton" onClick={displayChat}>{display ? "Close" :"Start a chat"}</button>
             </div>
